f8a0a4f7c81524e7676d8b118999f40f7b3615c6
carlos_costa <carloscosta.pen@gmail.com>
2026-03-24 11:28:08 -0300

    Add project files.

---
+++ DataAccessLayer/clsDataBaseAccess.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    internal class clsDataBaseAccess
+    {
+        static public string Access = "server = .; database = ContactsDB; user id= sa; password = 123456";
+    }
+}
