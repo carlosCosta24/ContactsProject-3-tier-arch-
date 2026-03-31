6906a58d037f39c89bff0be5f97ad08bdafdb535
carlos_costa <carloscosta.pen@gmail.com>
2026-03-31 00:03:46 -0300

    update Find fun

--- BusinessLayer/clsCountries.cs
+++ BusinessLayer/clsCountries.cs
@@ -1,72 +1,70 @@
 ﻿using DataAccessLayer;
-using System;
-using System.Collections.Generic;
 using System.Data;
 
 namespace BusinessLayer
 {
     public class clsCountries
     {
-       
-
-            public enum enMode { AddNew = 0, Update = 1 };
-            enMode Mode = enMode.AddNew;
-            public int ID { get; set; }
-            public string CountryName { get; set; }
-            public string CountryCode { get; set; }
-            public string CountryPhoneCode { get; set; }
 
 
+        public enum enMode { AddNew = 0, Update = 1 };
+        enMode Mode = enMode.AddNew;
+        public int ID { get; set; }
+        public string CountryName { get; set; }
+        public string CountryCode { get; set; }
+        public string CountryPhoneCode { get; set; }
 
-            public clsCountries()
+
+
+        public clsCountries()
+        {
+
+            this.ID = -1;
+            this.CountryName = "";
+            this.CountryCode = "";
+            this.CountryPhoneCode = "";
+            Mode = enMode.AddNew;
+
+        }
+
+        private clsCountries(int ID, string CountryName, string Code, string PhoneCode)
+        {
+
+            this.ID = ID;
+            this.CountryName = CountryName;
+            this.CountryCode = Code;
+            this.CountryPhoneCode = PhoneCode;
+            Mode = enMode.Update;
+        }
+        public static clsCountries FindCountry(int ID)
+        {
+
+            int CountryID = -1;
+            string CountryName = "";
+            string CountryCode = "";
+            string PhoneCode = "";
+
+            if (clsCountriesData.FindCountry(ID, ref CountryName, ref CountryCode, ref PhoneCode))
             {
 
-                this.ID = -1;
-                this.CountryName = "";
-                this.CountryCode = "";
-                this.CountryPhoneCode = "";
-                Mode = enMode.AddNew;
-
+                return new clsCountries(ID, CountryName, CountryCode, PhoneCode);
             }
-
-            private clsCountries(int ID, string CountryName, string Code, string PhoneCode)
+            else
             {
-
-                this.ID = ID;
-                this.CountryName = CountryName;
-                this.CountryCode = Code;
-                this.CountryPhoneCode= PhoneCode;
-                Mode = enMode.Update;
+                return null;
             }
-            public static clsCountries FindCountry(int ID)
-            {
 
-                int CountryID = -1;
-                string CountryName = "";
-                string CountryCode = "";
-                string PhoneCode = "";
-
-                if (clsCountriesData.FindCountry (ID, ref CountryName , ref CountryCode, ref PhoneCode ))
-                {
-
-                    return new clsCountries(ID, CountryName,CountryCode,PhoneCode);
-                }
-                else
-                {
-                    return null;
-                }
-
-            }
+        }
         public static clsCountries FindCountry(string Name)
         {
-            int CountryID = -1; 
+            int CountryID = -1;
             string Code = "";
             string PhoneCode = "";
 
             if (clsCountriesData.FindCountry(ref CountryID, Name, ref Code, ref PhoneCode))
             {
 
-                return new clsCountries(CountryID, Name,Code,PhoneCode);
+                return new clsCountries(CountryID, Name, Code, PhoneCode);
             }
             else
             {
@@ -76,69 +74,69 @@ namespace BusinessLayer
         }
 
         public bool Save()
+        {
+
+            switch (Mode)
             {
 
-                switch (Mode)
-                {
+                case enMode.AddNew:
+                    if (_AddNewCountry())
+                    {
 
-                    case enMode.AddNew:
-                        if (_AddNewCountry())
-                        {
-
-                            Mode = enMode.Update;
-                            return true;
-                        }
-                        else
-                        {
-                            return false;
-                        }
-                    case enMode.Update:
-                        return _UpdateCountry();
-
-                }
-                return false;
-
-
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                case enMode.Update:
+                    return _UpdateCountry();
 
             }
+            return false;
 
-            private bool _AddNewCountry()
-            {
-
-
-                this.ID = clsCountriesData.AddNewCountry(this.CountryName);
-                return (this.ID != -1);
-
-            }
-
-            private bool _UpdateCountry()
-            {
-
-                return clsCountriesData.UpdateCountry(this.ID, this.CountryName);
-
-            }
-
-            public static bool DeleteCountry(int ID)
-            {
-
-                return clsCountriesData.DeleteCountry(ID);
-            }
-            public static DataTable GetAllCountries()
-            {
-
-
-                return clsCountriesData.GetAllCountries();
-
-            }
-
-            public static bool IsExist(int ID)
-            {
-
-                return clsCountriesData.IsExist(ID);
-
-
-            }
 
 
         }
+
+        private bool _AddNewCountry()
+        {
+
+
+            this.ID = clsCountriesData.AddNewCountry(this.CountryName);
+            return (this.ID != -1);
+
+        }
+
+        private bool _UpdateCountry()
+        {
+
+            return clsCountriesData.UpdateCountry(this.ID, this.CountryName);
+
+        }
+
+        public static bool DeleteCountry(int ID)
+        {
+
+            return clsCountriesData.DeleteCountry(ID);
+        }
+        public static DataTable GetAllCountries()
+        {
+
+
+            return clsCountriesData.GetAllCountries();
+
+        }
+
+        public static bool IsExist(int ID)
+        {
+
+            return clsCountriesData.IsExist(ID);
+
+
+        }
+
+
+    }
 }
